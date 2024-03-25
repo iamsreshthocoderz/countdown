c97b164ca0ce741b6f0cd255cc70f47b4159ed1d
iamsreshthocoderz <sreshthot@gmail.com>
2024-03-26 02:49:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const targetDate = new Date('26 March 2024 00:01').getTime();
+//targetDate.setHours(0,0,0,0); // set to midnight if needed
+const counterdownInterval = setInterval(() =>{
+    const now  = new Date().getTime();
+    const timeRemaing = tergetDate - now;
+}
+)
+
+
+
+
+
